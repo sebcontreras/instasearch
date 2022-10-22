5ef21f8445274769268083d4c06ed034a488052a
sebcontreras <seb.contreras24@gmail.com>
2022-10-22 14:37:00 -0600

    Post link: open in new tab

--- src/Frontend/Post.js
+++ src/Frontend/Post.js
@@ -2,18 +2,16 @@ import React from "react";
 
 const Post = ({ img, date, keywords, imgAlt, link }) => {
     return (
-        <a href={link}>
-            <div className="post">
-                <div className="post-img-container">
-                    <img src={img} alt={imgAlt.original} />
-                </div>
-                <div className="post-info">
-                    <a href={link}>Go to Post</a>
-                    <p>Date: {date.month} {date.day}, {date.year}</p>
-                    <p>Keywords: {keywords && keywords.toString()}</p>
-                </div>
+        <div className="post">
+            <div className="post-img-container">
+                <img src={img} alt={imgAlt.original} />
             </div>
-        </a>
+            <div className="post-info">
+                <a href={link} target="_blank" rel="noreferrer noopener">Go to Post</a>
+                <p>Date: {date.month} {date.day}, {date.year}</p>
+                <p>Keywords: {keywords && keywords.toString()}</p>
+            </div>
+        </div>
     );
 }
 
